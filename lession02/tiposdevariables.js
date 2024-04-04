a12d1455d19b05c29b924697f0cf4310b7a909ea
MauroCiro0812 <mauricioc9410@gmail.com>
2024-04-03 19:58:06 -0500

    ejemplos

---
+++ lession02/tiposdevariables.js
@@ -0,0 +1,18 @@
+//Tipo de dato string
+var nombre = "Mauro";
+console.log(nombre);
+
+
+//Tipo de dato numerico
+var numero = 500;
+console.log(numero);
+
+
+//Tipo de dato object
+var objeto = {
+    nombre: "Mauro",
+    apellido: "Ciro",
+    tel: 329837434
+};
+
+console.log(objeto);
---
+++ lession03/tipe.js
@@ -0,0 +1,24 @@
+/*
+Ejemplos de 
+tipos de datos
+en JavaScript
+*/
+//Tipo de dato string
+var nombre = "Mauro";
+console.log(typeof nombre);
+
+//tipo de dato numerico
+var numero = 8;
+console.log(typeof numero);
+
+//tipo de dato boolean
+var bool = true;
+console.log(typeof bool);
+
+//tipo de dato object
+var object = {
+    nombre:"Mauro",
+    apellido:"Ciro",
+    tel: 3298475664
+};
+console.log(typeof object);
